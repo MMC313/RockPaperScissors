bd048c14d46100d33d4fdc7cb88ae5ac7152a247
MartianMooCow <NathenL2832@gmail.com>
2022-06-22 13:01:53 -0400

    add score counter and 5 point win message

--- rps-ui.js
+++ rps-ui.js
@@ -1,3 +1,8 @@
+let cScore = 0;
+let pScore = 0;
+
+
+
 function computerPlay(){
     let choice = ["Rock","Paper","Scissors"];
    return choice[Math.floor(Math.random()*3)];
@@ -25,12 +30,46 @@ function playRound(playerSelection,computerSelection){
 
 const buttons = document.querySelectorAll("button");
 const result = document.querySelector("#result");
+const cs = document.querySelector(".cscore");
+const ps = document.querySelector(".pscore");
+const message = document.querySelector(".message")
+
+
+ps.textContent=pScore;
+cs.textContent=cScore;
+
 
 buttons.forEach((button) => {
 
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
-      result.textContent = playRound(button.id);
+        result.textContent = playRound(button.id);
+        if(result.textContent.includes("win")){
+            pScore++;
+            ps.textContent=`Your score is ${pScore}`;
+        }else if(result.textContent.includes("lose")){
+            cScore++;
+            cs.textContent=`Computer score is ${cScore}`;
+        }
+        if(pScore === 5){
+            message.textContent="You win! You got 5 points first!"
+
+            
+        }else if(cScore === 5){
+            message.textContent="You lose! The computer got 5 points first!"
+
+        }
+      
+
     });
-  });
+    
+  
+});
+
+
+
+
+
+
+
 
